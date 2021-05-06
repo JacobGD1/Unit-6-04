5a3d9f180df4d9c8aa0bab6e76a0d0f852ad931b
Jacob Dominic <>
2021-05-06 16:13:55 +0000

    Complete File Change

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-let trapezoidBase1 = parseInt(prompt('Please enter base1 of trapezoid:'))
-let trapezoidBase2 = parseInt(prompt(
-  'lease enter base2 of trapezoid:'))
-let trapezoidHeight = parseInt(prompt('Please enter height of trapezoid:'))
-let areaOfTrapezoid = (trapezoidBase1 + trapezoidBase2) * trapezoidHeight / 2
-console.log('Area of trapezoid is:' + areaOfTrapezoid)
-// This shows the area of a trapezoid in your console log.
+let sideone = 0
+let sidetwo = 0
+let height = 0
+let number = 0
+// these are the variables for my answers.
+document.getElementById('calculate').addEventListener('click', buttonOne)
+// this event listener creates a button to calculate the area of the trapezoid.
+function buttonOne () {
+ sideone = document.getElementById('sideone').value
+ sideone = parseInt(sideone)
+ sidetwo = document.getElementById('sidetwo').value
+ sidetwo = parseInt(sidetwo)
+ height = document.getElementById('height').value
+ height = parseInt(height)
+ number = (sideone + sidetwo) / 2 * height
+document.getElementById('area').innerHTML = (number)
+}// this code allows the document to take side 1 and side 2 and divide them by 2, then it will take the number from that and multiply it by the height to give you the area of a trapezoid.
